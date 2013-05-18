caf1e87e35369eac0ed859aac774d139f468cc5b
Menghui Wang <wmhkebe@gmail.com>
2013-05-18 16:15:40 +0800

    login form

---
+++ app/template/login.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>Sign-in</title>
+    </head>
+    <body>
+        <h1>Sign in</h1>
+        <form method="post">
+            <p><label for="email">Email: </label><input id="email" type="text" name="email" /></p>
+            <p><label for="password">Password: </label><input id="password" type="password" name="password" /></p>
+            <input type="submit" value="Sign in" />
+        </form>
+    <body>
+</html>
