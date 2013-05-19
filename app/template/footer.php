c2a49f46ebb625cea9ce533075bda1c4c662d664
jun <qujun51319@gmail.com>
2013-05-19 13:08:21 +0800

    new about page

---
+++ app/template/footer.php
@@ -0,0 +1,5 @@
+<center>
+<footer id="page-footer">
+<p> Copyright &copy; 2013 <a href="http://github.com/furtherLee">Li Shijian</a>,<a href="http://github.com/qujun51319">Qu Jun</a>,<a href="http://github.com/kirakira">Wang Menghui</a>. All Rights Reserved.
+</footer>
+</center>
